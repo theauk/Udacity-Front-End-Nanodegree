fa01a4789728178b5c1722dd50af8c837298b773
theauk <49352969+theauk@users.noreply.github.com>
2020-04-29 21:40:26 +0200

    Update readme and add navbar build feature

--- Project 2 - Dynamic Landing Page/js/app.js
+++ Project 2 - Dynamic Landing Page/js/app.js
@@ -17,14 +17,29 @@
  * Define Global Variables
  * 
 */
-
-
+let navElems = document.querySelectorAll('[data-nav]');
+let navMenu = document.querySelectorAll('.navbar__menu');
+let navBar = document.getElementById('navbar__list');
+let section = document.querySelectorAll('section');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
+// Build the navigation bar when the DOM content is loaded
+window.addEventListener('DOMContentLoaded', navBarBuild);
+
+// Function to check if a section is in the viewport
+function inViewport(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bouding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bouding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
 
 
 /**
@@ -33,8 +48,31 @@
  * 
 */
 
-// build the nav
+// Build the nav
+function navBarBuild() {
 
+    let navFragment = document.createDocumentFragment();
+
+    for (let i = 0; i < navElems.length; i++) {
+
+        // Create a list node and link.
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+
+        // Find the current section name.
+        let sectionName = document.querySelectorAll('section h2')[i].textContent;
+
+        // Create a link.
+        a.appendChild(document.createTextNode(sectionName));
+        a.href = `#${navElems[i].id}`
+
+        // Append the a to the li and then the li to the fragment.
+        li.appendChild(a);
+        navFragment.appendChild(li);
+    }
+
+    navBar.appendChild(navFragment);
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,7 +83,7 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
@@ -53,5 +91,3 @@
 // Scroll to section on link click
 
 // Set sections as active
-
-
