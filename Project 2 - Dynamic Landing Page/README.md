f8e833672cc95d71e5e47640517e47782fddf664
theauk <49352969+theauk@users.noreply.github.com>
2020-04-30 15:14:56 +0200

    Fix active section, README, and navbar display

--- Project 2 - Dynamic Landing Page/README.md
+++ Project 2 - Dynamic Landing Page/README.md
@@ -1,8 +1,9 @@
 # Dynamic Landing Page
 
-Landing page converted from static to dynamic mainly via Javascript.
+Landing page converted from static to dynamic via Javascript.
 
 ## Includes:
-* Dynamic navigation section.
-* Section active state to show which section is being viewed.
-* Scroll to anchor.
+* Dynamically built navigation section.
+* Highlighting the active section in the viewport when scrolling.
+* Scroll to the appropriate section from the navigation menu.
+* Scroll to top button.
--- Project 2 - Dynamic Landing Page/js/app.js
+++ Project 2 - Dynamic Landing Page/js/app.js
@@ -17,18 +17,17 @@
  * Define Global Variables
  * 
 */
+
 let navElems = document.querySelectorAll('[data-nav]');
-let navMenu = document.querySelectorAll('.navbar__menu');
 let navBar = document.getElementById('navbar__list');
-let section = document.querySelectorAll('section');
+let sections = document.querySelectorAll('section');
+let scrollButton = document.getElementById('scrollButton');
+
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-// Build the navigation bar when the DOM content is loaded
-window.addEventListener('DOMContentLoaded', navBarBuild);
 
 // Function to check if a section is in the viewport
 function inViewport(elem) {
@@ -36,19 +35,17 @@ function inViewport(elem) {
     return (
         bounding.top >= 0 &&
         bounding.left >= 0 &&
-        bouding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
-        bouding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
     );
 }
-
-
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// Build the nav
+// Build the nav and scroll to functionality
 function navBarBuild() {
 
     let navFragment = document.createDocumentFragment();
@@ -62,9 +59,10 @@ function navBarBuild() {
         // Find the current section name.
         let sectionName = document.querySelectorAll('section h2')[i].textContent;
 
-        // Create a link.
+        // Create a link with section anchor and a class.
         a.appendChild(document.createTextNode(sectionName));
         a.href = `#${navElems[i].id}`
+        a.classList.add('menu__link');
 
         // Append the a to the li and then the li to the fragment.
         li.appendChild(a);
@@ -72,12 +70,41 @@ function navBarBuild() {
     }
 
     navBar.appendChild(navFragment);
+
+    // Removing the event listener after the navbar has been built
+    window.removeEventListener('DOMContentLoaded', navBarBuild);
 }
 
 // Add class 'active' to section when near top of viewport
+function changeActive() {
 
+    // Find the active section.
+    for (let i = 0; i < sections.length; i++) {
 
-// Scroll to anchor ID using scrollTO event
+        // If the section is in the viewport we change the state to active
+        // Otherwise we remove it
+        if (inViewport(sections[i])) {
+            sections[i].classList.add('your-active-class');
+        } else {
+            sections[i].classList.remove('your-active-class');
+        }
+    }
+}
+
+// Scroll to top function
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// Function to display/hide the scroll button
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
 
 
 /**
@@ -86,8 +113,11 @@ function navBarBuild() {
  *
 */
 
-// Build menu 
+// Build the navigation bar when the DOM content is loaded
+window.addEventListener('DOMContentLoaded', navBarBuild);
 
-// Scroll to section on link click
+// Event listener for scroll which fires the scroll button and active state functions
+window.addEventListener('scroll', () => { scrollFunction(); changeActive(); });
 
-// Set sections as active
+// Listening for clicks on the scroll button
+scrollButton.addEventListener('click', () => { scrollToTop() })
