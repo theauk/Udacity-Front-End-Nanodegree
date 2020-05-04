28628ae5841f0d4a3bc5e31d46e60a75ff7c138c
theauk <49352969+theauk@users.noreply.github.com>
2020-05-04 17:08:44 +0200

    Add routes and API integration

--- Project 3 - Weather Journal App/server.js
+++ Project 3 - Weather Journal App/server.js
@@ -2,18 +2,44 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`running on localhost: ${port}`);
+};
+
+// Get request
+app.get('/get', (request, response) => {
+    console.log('Server is sending: ' + projectData);
+    response.send(projectData);
+});
+
+// Post request
+app.post('/post', (request, response) => {
+    projectData['temperature'] = request.body.temperature;
+    projectData['date'] = request.body.date;
+    projectData['userResponse'] = request.body.userResponse;
+    console.log('Posted to server: ' + projectData);
+    response.send(projectData);
+})
--- Project 3 - Weather Journal App/website/app.js
+++ Project 3 - Weather Journal App/website/app.js
@@ -1,5 +1,92 @@
 /* Global Variables */
+const apiKey = '7b08b66db08e5e3614f8e466817a979a';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Event listeners
+document.getElementById('generate').addEventListener('click', performAction);
+
+// Callback function
+function performAction(e) {
+    const zip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+
+    // Get the weather data based on the user entry
+    getData(baseURL + zip + "&appid=" + apiKey).then(function (weather) {
+
+        // Format the data as JSON
+        let data = {
+            'temperature': weather.main.temp,
+            'date': newDate,
+            'userResponse': feelings
+        };
+        // Post the data to the server
+        return postData('/post', data);
+
+    }).then(function (weatherData) {
+        // Get the weather data from the server
+        return getData('/get');
+
+    }).then(function (responseData) {
+        // Update the UI
+        updateUI(responseData);
+        
+    }).catch((err) => {
+        // Ask for new input if necessary
+        errorFunction();
+    })
+}
+
+// Get
+const getData = async (url = '') => {
+    const res = await fetch(url)
+    try {
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
+// Post
+const postData = async (url = '', data = {}) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+// Update the UI
+const updateUI = async (data) => {
+
+    // Convert the temperature from Kelvin to C and F
+    const cTemp = (data.temperature - 273.15).toFixed(1);
+    const fTemp = (((data.temperature - 273.15) * (9 / 5)) + 32).toFixed(1);
+
+    // Display the data to the user
+    date.innerHTML = data.date;
+    temp.innerHTML = cTemp + ' °C / ' + fTemp + ' °F';
+    content.innerHTML = data.userResponse;
+}
+
+// Error function
+const errorFunction = async () => {
+    zip.value = '';
+    zip.placeholder = 'Please enter a valid US zip code';
+}
